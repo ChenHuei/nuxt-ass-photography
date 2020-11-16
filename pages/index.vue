a41963d57fa84d011428c4f30589f20a8d6f1d8c
Leo <leo.chen@cloud-interactive.com>
2020-11-16 18:44:34 +0800

    Add example for firebase

--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,23 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class IndexPage extends Vue {}
+export default class IndexPage extends Vue {
+  mounted() {
+    this.$fire.firestore
+      .collection('test')
+      .doc('jTC0nEEKNWCz9X9uQUj9')
+      .get()
+      .then((res) => {
+        console.log(res.data())
+      })
+
+    this.$fire.storage
+      .ref('test')
+      .child('test.jpeg')
+      .getDownloadURL()
+      .then((res) => {
+        console.log(res)
+      })
+  }
+}
 </script>
