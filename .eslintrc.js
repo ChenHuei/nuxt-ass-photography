3378b9865bafd718af0a58adaa778c7981de22d6
Leo <leo.chen@cloud-interactive.com>
2020-11-16 15:14:13 +0800

    Modify eslint and stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,41 @@ module.exports = {
     browser: true,
     node: true,
   },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
   extends: [
+    '@vue/standard',
+    '@vue/typescript',
     '@nuxtjs/eslint-config-typescript',
+    '@nuxtjs',
     'prettier',
     'prettier/vue',
+    'plugin:vue/recommended',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'nuxt', 'vue'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 'off',
+    'prettier/prettier': 'error',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+  },
 }
