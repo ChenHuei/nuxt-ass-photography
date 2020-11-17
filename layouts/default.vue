7edd3a2ce383bd8d0f0761c3dc33cb1d3ecdc0dc
Leo <leo.chen@cloud-interactive.com>
2020-11-17 16:05:49 +0800

    Modify default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="default-layout">
+    <layout-header />
+    <div class="default-layout-wrapper">
+      <div class="default-layout-container">
+        <Nuxt />
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+// components
+import LayoutHeader from '@/components/project/layout/Header.vue'
+
+@Component({
+  components: { LayoutHeader },
+})
+export default class DefaultLayout extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.default-layout {
+  @include size(100vw, 100vh);
+
+  display: felx;
+  flex-direction: column;
+  color: var(--white);
+  background-color: var(--primary);
+}
+</style>
