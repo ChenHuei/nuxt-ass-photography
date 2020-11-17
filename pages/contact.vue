d04f0f7df4ebf3d8623963e254692097b3f878e2
Leo <leo.chen@cloud-interactive.com>
2020-11-17 16:05:20 +0800

    Add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="contact">contact</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ContactPage extends Vue {}
+</script>
