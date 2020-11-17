28e5a10b5c52bdce006c0b1fa56476a40cbb79c2
Leo <leo.chen@cloud-interactive.com>
2020-11-17 11:23:38 +0800

    Add firebase query example

--- pages/index.vue
+++ pages/index.vue
@@ -9,20 +9,46 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class IndexPage extends Vue {
   mounted() {
     this.$fire.firestore
-      .collection('test')
-      .doc('jTC0nEEKNWCz9X9uQUj9')
+      .collection('album')
       .get()
       .then((res) => {
-        console.log(res.data())
+        res.docs.forEach((item) => {
+          item.data().photos.map((id: string) => {
+            return this.$fire.firestore
+              .collection('photo')
+              .doc(id)
+              .get()
+              .then((image) => {
+                console.log(image.data())
+              })
+          })
+        })
       })
+    // .collection('album')
+    // .doc()
+    // .get()
+    // .then((res) => {
+    //   console.log(res.data())
+    // })
+    // .doc('jTC0nEEKNWCz9X9uQUj9')
+    // .get()
+    // .then((res) => {
+    //   console.log(res.data())
+    // })
 
-    this.$fire.storage
-      .ref('test')
-      .child('test.jpeg')
-      .getDownloadURL()
-      .then((res) => {
-        console.log(res)
-      })
+    // this.$fire.storage
+    //   .ref()
+    //   .listAll()
+    //   .then((res) => {
+    //     res.prefixes.forEach((folder) => {
+    //       console.log(folder)
+    //     })
+    //     res.items.forEach((photo) => {
+    //       photo.getDownloadURL().then((url) => {
+    //         console.log(url)
+    //       })
+    //     })
+    //   })
   }
 }
 </script>
